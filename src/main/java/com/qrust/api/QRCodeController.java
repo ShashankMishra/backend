a6b768c1048837f82aa0310f59ce22da457a775e
Shashank Mishra <s.mishra.ext@sportradar.com>
2025-06-17 22:48:53 +0530

    feat: change redirect path

--- src/main/java/com/qrust/api/QRCodeController.java
+++ src/main/java/com/qrust/api/QRCodeController.java
@@ -100,7 +100,7 @@ public class QRCodeController {
         ScanHistory scanHistory = createScanHistory(qrId, headers, uriInfo);
 
         URI redirectUrl = UriBuilder.fromUri(frontendUri)
-                .path("qr")
+                .path("scans")
                 .path(scanHistory.getScanId().toString())
                 .build();
 
